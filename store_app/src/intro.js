eb1d147ea66828c050cd7720ecae9ccb0a0a8a73
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-03-18 15:49:26 -0700

    Remove store_app project files including package.json, TypeScript configuration, source files, and assets

--- store_app/src/intro.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.intro = intro;
-function intro() {
-    var user = { name: "Tobechi", age: 10 };
-    console.log("Tobechi");
-    console.log(user.name);
-}
--- store_app/src/intro.ts
+++
@@ -1,7 +0,0 @@
-export function intro(){
-  let user = { name: "Tobechi", age: 10}
-
-  console.log("Tobechi");
-  // console.log(user.name);
-  console.log(user.email);
-}
